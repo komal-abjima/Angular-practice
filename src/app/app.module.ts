f5e6197e127d5814b4118399aba396e9acd4e5a6
komal-abjima <153979765+komal-abjima@users.noreply.github.com>
2024-01-24 18:16:10 +0530

    initial-commit(24-01-24)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -12,13 +10,30 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'servers', component: ServersComponent}
-];
+// const appRoutes: Routes = [
+//   {path: '', component: HomeComponent},
+//   {path: 'users', component: UsersComponent, children:[
+//       {path: ':id/:name', component: UserComponent}
+//   ]},
+//   // it is dynamically route the path
+//   // {path: 'users/:id/:name', component: UserComponent},
+//   {path: 'servers', component: ServersComponent, children:[
+//     {path: ':id', component: ServerComponent},
+//     // passing query parameters and fragments
+//     {path: ':id/edit',component: EditServerComponent }
+//   ]},
+//   {path: 'not-found', component: PageNotFoundComponent},
+//   // {path: 'something', redirectTo: '/not-found'}
+//   {path: '**', redirectTo: '/not-found'}
+// ];
+// {path: 'servers/:id', component: ServerComponent},
+// passing query parameters and fragments
+// {path: 'servers/:id/edit',component: EditServerComponent }
+// ]},
 
 
 @NgModule({
@@ -29,12 +44,14 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
+    // RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,22 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    // it default taking id as a string so we convert it into a number by using + sign
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(1);
+    this.route.params
+    .subscribe((params: Params) =>{
+      this.server = this.serversService.getServer(+params['id']);
+    })
+  }
+
+  editBtn(){
+    this.router.navigate(['edit'], {relativeTo:this.route, queryParamsHandling: 'preserve'});
   }
 
 }
