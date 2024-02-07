fe98534bfa2ba4bc39a5bb41c772de074b653f60
komal-abjima <153979765+komal-abjima@users.noreply.github.com>
2024-02-07 18:27:33 +0530

    initial-commit(07-02-24)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'food-app';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts()
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    
+    console.log(postData);
+    this.http.post('https://angularhttp-ce0f8-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData=>{
+      console.log(responseData);
+    })
+  }
+
+  onFetchPosts() {
+    this.fetchPosts()
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get('https://angularhttp-ce0f8-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData =>{
+      const postArray = [];
+      for(const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          postArray.push({...responseData[key], id: key})
+        }
+      }
+      return postArray;
+
+    }))
+    .subscribe(posts =>{
+      console.log(posts);
+    })
+  }
 }
